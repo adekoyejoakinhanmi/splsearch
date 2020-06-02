761573d61d6b160886964f3b4cdeff0317b767b7
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2020-06-03 00:23:25 +0100

    remove env

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const ACCESS_KEY = 'cf57663eec5448ba74ddb9fce13cb4a4c0de6773441049147485d5a62fe816f4';
-// const SECRET_KEY = '19a9b5ffa3b63b0f09cf4be8b91202fb3246f91c33c8d040bfa77306903e015f';
+const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 export const getImages = term => {
   return axios({
